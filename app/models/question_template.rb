d299179fd750e3cb1d6ec556ef112423863323d6
thomas mayrhofer <thomas@mayrhofer.at>
2014-04-28 15:37:44 +0200

    added replacement functions

--- app/models/question_template.rb
+++ app/models/question_template.rb
@@ -2,9 +2,18 @@ class QuestionTemplate < ActiveRecord::Base
   has_many :questions
   validates :question,  presence: true
 
+  private
   def extract_placeholder
-    question   = self.question
-    question.scan(/\?\S*/).flatten
+    question = self.question
+    question.scan(regex_placeholder).flatten
   end
 
+  def replace_placeholder(replace)
+    question  = self.question
+    question.gsub regex_placeholder, replace
+  end
+
+  def regex_placeholder
+    Regexp.new '\?\S*'
+  end
 end
--- test/models/question_template_test.rb
+++ test/models/question_template_test.rb
@@ -6,10 +6,24 @@ class QuestionTemplateTest < ActiveSupport::TestCase
   end
 
   test "extract_params_func" do
-    result = @question.extract_placeholder
+    result = @question.send :extract_placeholder
 
     assert_not_nil result
     assert_equal result.class, Array
   end
 
+  test "replace placeholder" do
+    result = @question.send :extract_placeholder
+    question_cache = @question.question
+    r  = {}
+    result.each do |placeholder|
+      r[placeholder] = LoremIpsum.lorem_ipsum words: 1
+    end
+
+    new_question = @question.send :replace_placeholder, r
+
+    assert_not_equal new_question, question_cache
+    assert_nil new_question.match(@question.send :regex_placeholder)
+    assert_not_nil question_cache.match(@question.send :regex_placeholder)
+  end
 end
