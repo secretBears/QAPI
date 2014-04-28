f130debe400c36f7fa35380b3b0fb68aeb6233b9
thomas mayrhofer <thomas@mayrhofer.at>
2014-04-28 17:47:14 +0200

    rubocop fix

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,6 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 
-
 Place.create!(longitude: 13.2, latitude: 47.48, name: "Salzburg")
 Place.create!(longitude: 16.22, latitude: 48.12, name: "Wien")
 Place.create!(longitude: 14.18, latitude: 48.18, name: "Linz")
