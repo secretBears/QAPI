0c640f7e2b1a1267e64e1045830ef9a8fe9452fe
thomas mayrhofer <thomas@mayrhofer.at>
2014-05-03 17:14:53 +0200

    changed to sandbox url

--- config/initializers/freebase.rb
+++ config/initializers/freebase.rb
@@ -1,4 +1,4 @@
 # this file initializes the freebase api
 
 fail 'google api key not found' if ENV['GOOGLE_API_KEY'].nil?
-FreebaseAPI.session = FreebaseAPI::Session.new(key: ENV['GOOGLE_API_KEY'], env: :stable)
+FreebaseAPI.session = FreebaseAPI::Session.new(key: ENV['GOOGLE_API_KEY'], env: :sandbox)
