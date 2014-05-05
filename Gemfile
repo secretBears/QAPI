d13439b704f8776a7f4b87f8c4d4672a8f5c1683
thomas mayrhofer <thomas@mayrhofer.at>
2014-05-05 22:54:06 +0200

    removed rubocop

--- Gemfile
+++ Gemfile
@@ -2,10 +2,7 @@ source 'https://rubygems.org'
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '4.0.1'
-
-# Use sqlite3 as the database for Active Record
-gem 'sqlite3', group: [:development, :test]
-gem 'pg', group: [:production]
+gem 'pg'
 
 
 # Use SCSS for stylesheets
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20_140_505_181_743) do
+ActiveRecord::Schema.define(version: 20140505181743) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
 
   create_table "answers", force: true do |t|
     t.text    "answer"
@@ -19,7 +22,7 @@ ActiveRecord::Schema.define(version: 20_140_505_181_743) do
     t.boolean "is_true"
   end
 
-  add_index "answers", ["question_id"], name: "index_answers_on_question_id"
+  add_index "answers", ["question_id"], name: "index_answers_on_question_id", using: :btree
 
   create_table "places", force: true do |t|
     t.float    "latitude"
@@ -54,7 +57,7 @@ ActiveRecord::Schema.define(version: 20_140_505_181_743) do
     t.integer  "question_template_id"
   end
 
-  add_index "questions", ["place_id"], name: "index_questions_on_place_id"
+  add_index "questions", ["place_id"], name: "index_questions_on_place_id", using: :btree
 
   create_table "users", force: true do |t|
     t.datetime "created_at"
@@ -71,7 +74,7 @@ ActiveRecord::Schema.define(version: 20_140_505_181_743) do
     t.string   "last_sign_in_ip"
   end
 
-  add_index "users", ["email"], name: "index_users_on_email", unique: true
-  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
 end
