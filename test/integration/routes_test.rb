26abbbe6cdf199b3b3dc293096b80c6089f868cf
Alexander Junger <hello@alexanderjunger.at>
2014-05-05 16:52:59 +0200

    updated integration and model tests to match new routes/names etc...

--- test/integration/routes_test.rb
+++ test/integration/routes_test.rb
@@ -2,13 +2,11 @@ require 'test_helper'
 
 class RoutesTest < ActionDispatch::IntegrationTest
   test 'test output format' do
-    assert_generates '/api/question/one', controller: 'questions', action: 'show', id: 'one', format: :json
-    assert_generates '/api/question/-1',  controller: 'questions', action: 'show', id: '-1', format: :json
-    assert_generates '/api/question',     controller: 'questions', action: 'show_random', format: :json
+    assert_generates '/api',     controller: 'questions', action: 'show_random', format: :json
   end
 
   test 'should get show_lat_long' do
-    assert_generates('/api/question/47.8094888/13.0550007',
+    assert_generates('/api/47.8094888/13.0550007',
                      controller: 'questions',
                      action: 'show_lat_long',
                      latitude: 47.8094888,
--- test/models/question_test.rb
+++ test/models/question_test.rb
@@ -11,7 +11,7 @@ class QuestionTest < ActiveSupport::TestCase
 
   test 'should get question to lat long' do
     place = Place.find 1
-    question = Question.get_from_lat_long latitude: place.latitude, longitude: place.longitude
+    question = Question.find_by_lat_long latitude: place.latitude, longitude: place.longitude
     assert_not_nil question
   end
 
