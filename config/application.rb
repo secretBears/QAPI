9de8403548b5367553c6abd24a08a108e07b05eb
thomas mayrhofer <thomas@mayrhofer.at>
2014-05-05 21:05:00 +0200

    removed ssl encryption

--- config/application.rb
+++ config/application.rb
@@ -20,5 +20,6 @@ module QAPI
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
     config.i18n.enforce_available_locales = false
+    config.force_ssl = false
   end
 end
