f947c57560bd9fe3ad9c300b790c4644cecc9247
thomas mayrhofer <thomas@mayrhofer.at>
2014-05-06 13:12:04 +0200

    changed travis yml

--- app/models/generators/answer_generator.rb
+++ app/models/generators/answer_generator.rb
@@ -2,4 +2,8 @@ class AnswerGenerator < AbstractGenerator
   def initialize(arguments)
     super arguments
   end
+
+  def generate(query)
+
+  end
 end
--- test/models/answer_generator_test.rb
+++ test/models/answer_generator_test.rb
@@ -2,9 +2,14 @@ require 'test_helper'
 
 class QuestionGeneratorTest < ActiveSupport::TestCase
   setup do
+    @query =
     @question_generator = AnswerGenerator.new(
         location: 'Salzburg',
         template: 'Welchen Beruf hatte ?name'
     )
   end
+  #
+  # test "should get answers" do
+  #   @question_generator.generate
+  # end
 end
