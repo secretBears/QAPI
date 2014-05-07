430958411563140118d4cf6964f201aa413f9faa
Alexander Junger <hello@alexanderjunger.at>
2014-05-07 15:38:16 +0200

    removed Question.random_question method and corresponding routes, actions and tests

--- test/integration/routes_test.rb
+++ test/integration/routes_test.rb
@@ -1,10 +1,6 @@
 require 'test_helper'
 
 class RoutesTest < ActionDispatch::IntegrationTest
-  test 'test output format' do
-    assert_generates '/api',     controller: 'questions', action: 'show_random', format: :json
-  end
-
   test 'should get show_lat_long' do
     assert_generates('/api/47.8094888/13.0550007',
                      controller: 'questions',
--- test/models/question_test.rb
+++ test/models/question_test.rb
@@ -1,18 +1,9 @@
 require 'test_helper'
 
 class QuestionTest < ActiveSupport::TestCase
-  test 'get random question id' do
-    (0..50).each do
-      element = Question.random_question
-      assert_not_nil element['id']
-      assert_not_nil element['question']
-    end
-  end
-
   test 'should get question to lat long' do
     place = Place.find 1
     question = Question.find_by_lat_long latitude: place.latitude, longitude: place.longitude
     assert_not_nil question
   end
-
 end
