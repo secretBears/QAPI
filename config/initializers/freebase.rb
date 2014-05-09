a12a8ffa935961ab2abb4446beacb9d66f286767
thomas mayrhofer <thomas@mayrhofer.at>
2014-05-09 13:09:00 +0200

    see if env variables are defined

--- config/initializers/freebase.rb
+++ config/initializers/freebase.rb
@@ -1,4 +1,9 @@
 # this file initializes the freebase api
+puts "---------------"
+puts ENV['GOOGLE_API_KEY'].nil?
+puts ENV['GOOGLE_API_KEY']
+puts "---------------"
+
 
 fail 'google api key not found' if ENV['GOOGLE_API_KEY'].nil?
 FreebaseAPI.session = FreebaseAPI::Session.new(key: ENV['GOOGLE_API_KEY'], env: :stable)
