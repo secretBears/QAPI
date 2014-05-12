d54f11d76475cd66e0def62dbf02e293d0d42ee9
Alexander Junger <hello@alexanderjunger.at>
2014-05-13 00:50:59 +0200

    validates presence of query param

--- app/models/question_template.rb
+++ app/models/question_template.rb
@@ -2,5 +2,5 @@ class QuestionTemplate < ActiveRecord::Base
   has_many :questions
   has_many :question_placeholders
 
-  validates :question,  presence: true
+  validates :question, :query,  presence: true
 end
