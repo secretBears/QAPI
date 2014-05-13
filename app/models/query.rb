c02385f371a8ee8f4305e84757a4919c91442765
Alexander Junger <hello@alexanderjunger.at>
2014-05-13 15:38:18 +0200

    added parse to hash before save and output of query in no result error in query model

--- app/models/query.rb
+++ app/models/query.rb
@@ -3,6 +3,9 @@ class Query < ActiveRecord::Base
   validates_presence_of :query_hash, :location_property, :answer_property
   serialize :query_hash, Hash
 
+  before_save :parse_hash
+  #after_find :stringify_hash
+
   def get(location)
     query = self[:query_hash].clone
     query[self[:location_property]] = location
@@ -14,9 +17,19 @@ class Query < ActiveRecord::Base
   end
 
   private
+  def parse_hash
+    if query_hash.acts_like_string?
+      self.query_hash = JSON.parse(self.query_hash)
+    end
+  end
+
+  def stringify_hash
+    self.query_hash = JSON.unparse(self.query_hash)
+  end
+
   def fire_query(query)
     result = FreebaseAPI.session.mqlread query
-    fail Exceptions::QueryNotFound, 'No Results for query' if result.nil?
+    fail Exceptions::QueryNotFound, 'No Results for query' + query.to_s if result.nil?
     result
   end
 end
