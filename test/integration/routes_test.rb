8da85add4f59fa94f23ef9deea21bc763a42abdf
thomas mayrhofer <thomas@mayrhofer.at>
2014-05-13 20:08:57 +0200

    added routes tests for public api

--- test/integration/routes_test.rb
+++ test/integration/routes_test.rb
@@ -1,6 +1,13 @@
 require 'test_helper'
 
 class RoutesTest < ActionDispatch::IntegrationTest
+  test 'should get index' do
+    assert_generates('/',
+                     controller: 'static',
+                     action: 'index'
+    )
+  end
+
   test 'should get show_lat_long' do
     assert_generates('/api/47.8094888/13.0550007',
                      controller: 'questions',
@@ -24,5 +31,39 @@ class RoutesTest < ActionDispatch::IntegrationTest
                      action: 'edit',
                      id: '1'
     )
+  end
+
+  test 'should get all api paths' do
+    assert_routing({
+                       path: 'api',
+                       method: :get
+                   }, {
+                       controller: 'questions',
+                       action: 'random',
+                       format: 'json'
+                   })
+
+    assert_routing({
+                       path: 'api/47.8094888/13.0550007',
+                       method: :get
+                   }, {
+                       controller: 'questions',
+                       action: 'show_lat_long',
+                       latitude: "47.8094888",
+                       longitude: "13.0550007",
+                       format: 'json'
+                   })
+
+    assert_routing({
+                       path: 'api/47.8094888/13.0550007/2',
+                       method: :get
+                   }, {
+                       controller: 'questions',
+                       action: 'show_lat_long',
+                       format: 'json',
+                       latitude: "47.8094888",
+                       longitude: "13.0550007",
+                       count: "2"
+                   })
   end
 end
