751f872ea4c7fae8e01c883037a9c9d826d0bca5
Alexander Junger <hello@alexanderjunger.at>
2014-05-14 16:05:21 +0200

    raising exception and rendering error layout instead of directly rendering 401

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,19 +8,21 @@ class ApplicationController < ActionController::Base
 
   rescue_from Exceptions::PlaceNotFound, with: :render_error
   rescue_from ActiveRecord::RecordNotFound, with: :render_error
+  rescue_from Exceptions::InvalidTokenError, with: :render_authentication_error
 
   private
   def render_error(message)
     render template: 'layouts/error', formats: 'json', locals: {message: message.to_s}, status: 404
   end
 
+  def render_authentication_error(message)
+    render template: 'layouts/error', formats: 'json', locals: {message: message.to_s}, status: 401
+  end
+
   protect_from_forgery with: :exception
 
   def restrict_access
-    unless restrict_access_by_params || restrict_access_by_header
-      render json: {message: 'Invalid API Token'}, status: 401
-      return
-    end
+    fail Exceptions::InvalidTokenError unless restrict_access_by_params || restrict_access_by_header
 
     @current_user = @api_key.user if @api_key
   end
