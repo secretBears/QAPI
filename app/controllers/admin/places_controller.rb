6b0f0cfe9249a03fd594440626bbd77182c8921b
Alexander Junger <hello@alexanderjunger.at>
2014-05-14 12:33:45 +0200

    changed controller inheritance

--- app/controllers/admin/places_controller.rb
+++ app/controllers/admin/places_controller.rb
@@ -1,4 +1,4 @@
-class Admin::PlacesController < Admin::ApplicationController
+class Admin::PlacesController < Admin::AdminController
   before_action :set_place, only: [:show, :edit, :update, :destroy]
 
   # GET /places
