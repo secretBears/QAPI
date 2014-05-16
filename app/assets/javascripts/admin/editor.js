f9f6e820a4f32099ab2cd29ef16f965f4ff9d8cd
Alexander Junger <hello@alexanderjunger.at>
2014-05-16 13:25:14 +0200

    calling test query init on page:change of turbolinks

--- app/assets/javascripts/admin/editor.js
+++ app/assets/javascripts/admin/editor.js
@@ -1,34 +1,34 @@
 function initPage() {
-        var container = $("#jsoneditor")[0];
-        var editor = new jsoneditor.JSONEditor(container);
+    var container = $("#jsoneditor")[0];
+    var editor = new jsoneditor.JSONEditor(container);
 
-        /**
-         * Before form is submitted,
-         */
-        $('.question_template_form').on('submit', function(e) {
-            e.preventDefault();
-            extractJson();
-            this.submit();
-        });
+    /**
+     * Before form is submitted,
+     */
+    $('.question_template_form').on('submit', function(e) {
+        e.preventDefault();
+        extractJson();
+        this.submit();
+    });
 
-        /**
-         * Parse value of query_hash field and set it as content of the editor.
-         */
-        function infuseJson() {
-            editor.set(JSON.parse($('#query_hash').val()));
-        };
+    /**
+     * Parse value of query_hash field and set it as content of the editor.
+     */
+    function infuseJson() {
+        editor.set(JSON.parse($('#query_hash').val()));
+    };
 
-        /**
-         * Stringify the content of the editor and set it as value of query_hash field.
-         */
-        function extractJson() {
-            $('#query_hash').val(JSON.stringify(editor.get()));
-        };
+    /**
+     * Stringify the content of the editor and set it as value of query_hash field.
+     */
+    function extractJson() {
+        $('#query_hash').val(JSON.stringify(editor.get()));
+    };
 
 
-        window.QAPI = window.QAPI || {};
-        window.QAPI.editor = editor;
-        window.QAPI.extractFromEditor = extractJson;
+    window.QAPI = window.QAPI || {};
+    window.QAPI.editor = editor;
+    window.QAPI.extractFromEditor = extractJson;
 
     infuseJson();
 };
