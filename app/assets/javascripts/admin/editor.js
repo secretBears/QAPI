b2c43ad2503e7258c6a9f50699f533ea168c7e22
Alexander Junger <hello@alexanderjunger.at>
2014-05-16 12:22:21 +0200

    moved code out of $(document).ready() to be called also when turbolinks reloads content

--- app/assets/javascripts/admin/editor.js
+++ app/assets/javascripts/admin/editor.js
@@ -1,62 +1,35 @@
+function initPage() {
+        var container = $("#jsoneditor")[0];
+        var editor = new jsoneditor.JSONEditor(container);
 
-$(document).ready(function() {
+        /**
+         * Before form is submitted,
+         */
+        $('.edit_question_template').on('submit', function(e) {
+            e.preventDefault();
+            extractJson();
+            this.submit();
+        });
 
-    var t = '', activeProperty;
-    var locationProp = document.getElementById('location_property'),
-        answerProp = document.getElementById('answer_property');
+        /**
+         * Parse value of query_hash field and set it as content of the editor.
+         */
+        function infuseJson() {
+            editor.set(JSON.parse($('#query_hash').val()));
+        };
 
-    function gText(e) {
-        t = (document.all) ? document.selection.createRange().text : document.getSelection();
-
-        activeProperty.value = t;
-    }
-
-    function setActiveProperty(input) {
-        $(activeProperty).removeClass('activeProperty')
-        activeProperty = input;
-        $(activeProperty).addClass('activeProperty')
-    }
-
-    locationProp.onfocus = function() {
-        setActiveProperty(locationProp);
-    }
-
-    answerProp.onfocus = function() {
-        setActiveProperty(answerProp);
-    }
-
-    $('query_hash').on('mouseup', gText);
-
-    var container = document.getElementById("jsoneditor");
-    var editor = new jsoneditor.JSONEditor(container);
-
-    /**
-     * Before form is submitted,
-     */
-    $('.edit_question_template').submit(function(e) {
-        e.preventDefault();
-        extractJson();
-        this.submit();
-    });
-
-    /**
-     * Parse value of query_hash field and set it as content of the editor.
-     */
-    function infuseJson() {
-        editor.set(JSON.parse($('#query_hash').val()));
-    };
-
-    /**
-     * Stringify the content of the editor and set it as value of query_hash field.
-     */
-    function extractJson() {
-        $('#query_hash').val(JSON.stringify(editor.get()));
-    };
-
-    infuseJson();
+        /**
+         * Stringify the content of the editor and set it as value of query_hash field.
+         */
+        function extractJson() {
+            $('#query_hash').val(JSON.stringify(editor.get()));
+        };
 
 
-    window.QAPI = window.QAPI || {};
-    window.QAPI.editor = editor;
-    window.QAPI.extractFromEditor = extractJson;
-});
+        window.QAPI = window.QAPI || {};
+        window.QAPI.editor = editor;
+        window.QAPI.extractFromEditor = extractJson;
+};
+
+$(window).on('page:change', initPage);
+$(document).on('ready', initPage);
