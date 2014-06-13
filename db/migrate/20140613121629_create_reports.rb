7f3f12ad3f85176266a6796475bcdc04d5b4b0ed
Alexander Junger <hello@alexanderjunger.at>
2014-06-13 16:23:43 +0200

    implemented reporting feature

--- db/migrate/20140613121629_create_reports.rb
+++ db/migrate/20140613121629_create_reports.rb
@@ -1,11 +1,10 @@
 class CreateReports < ActiveRecord::Migration
   def change
     create_table :reports do |t|
-      t.integer, :user_id
-      t.integer, :question_id
-      t.string, :comment
+      t.integer :user_id
+      t.integer :question_id
       t.string :problem
-
+      t.string :comment
       t.timestamps
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140519185636) do
+ActiveRecord::Schema.define(version: 20140613123819) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -74,6 +74,14 @@ ActiveRecord::Schema.define(version: 20140519185636) do
   end
 
   add_index "questions", ["place_id"], name: "index_questions_on_place_id", using: :btree
+
+  create_table "reports", force: true do |t|
+    t.integer "question_id"
+    t.string "problem"
+    t.string "comment"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", force: true do |t|
     t.datetime "created_at"
