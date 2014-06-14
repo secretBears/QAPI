92a3aa7d289fc2a8f50efc549b2a35b801198add
Alexander Junger <hello@alexanderjunger.at>
2014-06-14 12:58:48 +0200

    config.assets.initialize_on_precompile set to false in an attempt to let travis pass

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -22,6 +22,8 @@ QAPI::Application.configure do
   # Disable Rails's static asset server (Apache or nginx will already do this).
   config.serve_static_assets = true
 
+  config.assets.initialize_on_precompile = false
+
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
   # config.assets.css_compressor = :sass
