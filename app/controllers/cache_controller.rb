65eec242697cf2ceb7711e29773e329639f2eeea
thomas mayrhofer <thomas@mayrhofer.at>
2014-06-14 16:45:38 +0200

    rubocop autofix

--- app/controllers/cache_controller.rb
+++ app/controllers/cache_controller.rb
@@ -1,6 +1,6 @@
 class CacheController < ApplicationController
   before_action :authorize_admin
-  after_action  :render_nothing
+  after_action :render_nothing
 
   def clear_all
     Cache.clear_all
